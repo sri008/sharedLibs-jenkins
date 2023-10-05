faa189d00de8cc49a9a6c6215058541115f4c7e6
Srikant Dhoundiyal <32190963+sri008@users.noreply.github.com>
2023-10-05 19:22:37 +0530

    Update cronJob.groovy

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -1,5 +1,6 @@
 def call(body) {
     body()
+    // cant use thi below method as it should be declare in another file as body() method is used
     // def getCronParams() {
     //     if(env.GIT_URL.contains('infra') ) {
     //         return 'H */4 * * 1-5'
@@ -11,15 +12,9 @@ def call(body) {
     
     pipeline {
         agent any
-        def cronValue
-        if (env.GIT_URL.contains('infra') || env.GIT_URL.contains('infrastructure')) {
-            cronValue = 'H */4 * * 1-5'
-        } else {
-            cronValue = ''
-        }
         triggers { 
-            // cron( env.BRANCH_NAME == 'main' && env.GIT_URL.contains('infra') == 'true' ? '0 1 * * 1' : '')
-            cron( cronValue)
+            cron( env.BRANCH_NAME == 'main' && (env.GIT_URL.contains('infra') || env.GIT_URL.contains('infrastructure')) ? '0 1 * * 1' : '')
+            // cron( cronValue)
         }
         stages {
             stage('github url') {
