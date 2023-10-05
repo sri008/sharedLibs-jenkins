834d249e8b67a69b54ff68db176509791d8123f7
Srikant Dhoundiyal <32190963+sri008@users.noreply.github.com>
2023-10-05 18:51:12 +0530

    Update cronJob.groovy

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -1,14 +1,18 @@
 def call(body) {
     body()
+    def getCronParams() {
+        if(env.GIT_URL.contains('infra') ) {
+            return 'H */4 * * 1-5'
+        } 
+        else {
+            return ''
+        }
+    }
     pipeline {
         agent any
         triggers { 
             // cron( env.BRANCH_NAME == 'main' && env.GIT_URL.contains('infra') == 'true' ? '0 1 * * 1' : '')
-            script {
-                if (env.BRANCH_NAME == 'main' && env.GIT_URL.contains('infra')) {
-                    cron('0 1 * * 1')
-                }
-            }
+            cron( getCronParams())
         }
         stages {
             stage('github url') {
