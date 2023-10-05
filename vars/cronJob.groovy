83a5836513ec73f213a88d340c1b442feef23605
Srikant Dhoundiyal <32190963+sri008@users.noreply.github.com>
2023-10-05 21:54:32 +0530

    Update cronJob.groovy

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -6,14 +6,15 @@ def call(body) {
     pipeline {
         agent any
         triggers { 
-            //cron( env.BRANCH_NAME == 'main' && (env.GIT_URL.contains('infra') || env.GIT_URL.contains('infrastructure')) ? '0 1 * * 1' : '')
-            cron( (gitURL.contains('infra') || gitURL.contains('infrastructure')) ? 'H 1 * * 1' : '')
+            cron( env.BRANCH_NAME == 'main' && (env.GIT_URL.contains('infra') || env.GIT_URL.contains('infrastructure')) ? '0 1 * * 1' : '')
+            // cron( (gitURL.contains('infra') || gitURL.contains('infrastructure')) ? 'H 1 * * 1' : '')
         }
         stages {
             stage('github url') {
                 steps {
-                    echo "THE Branch is use ==> ${branchName}"
-                    echo "the gitlhub repo url use ==> ${gitURL}"
+                    echo "${env.GIT_URL}"
+                    // echo "THE Branch is use ==> ${branchName}"
+                    // echo "the gitlhub repo url use ==> ${gitURL}"
                 }
             }
         }
