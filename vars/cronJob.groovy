0fac9568c9f3374c8ddf143f587e9dcf31624080
Srikant Dhoundiyal <32190963+sri008@users.noreply.github.com>
2023-10-05 20:12:14 +0530

    Update cronJob.groovy

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -1,15 +1,5 @@
 def call(body) {
     body()
-    // cant use thi below method as it should be declare in another file as body() method is used
-    // def getCronParams() {
-    //     if(env.GIT_URL.contains('infra') ) {
-    //         return 'H */4 * * 1-5'
-    //     } 
-    //     else {
-    //         return ''
-    //     }
-    // }
-    
     pipeline {
         agent any
         // triggers { 
