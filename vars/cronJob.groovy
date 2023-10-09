8f0a8a6faf7d6e04ddb33b38a79056910ff5c0e6
Srikant Dhoundiyal <32190963+sri008@users.noreply.github.com>
2023-10-09 12:05:27 +0530

    add the environment

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -1,16 +1,15 @@
 // def call(Map params = [:]) {
 def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    config.each { print(it) }
     pipeline {
         agent any
+        environment{
+            GIT_URL = 'https://github.com/sri008/testinfra.git'
+            BRANCH_NAME = 'dev'
+        }
         triggers { 
-            // cron( env.BRANCH_NAME == 'main' && (env.GIT_URL.contains('infra') || env.GIT_URL.contains('infrastructure')) ? '0 1 * * 1' : '')
+            cron( env.BRANCH_NAME == 'main' && (env.GIT_URL.contains('infra') || env.GIT_URL.contains('infrastructure')) ? '0 1 * * 1' : '')
             // cron( (gitURL.contains('infra') || gitURL.contains('infrastructure')) ? 'H 1 * * 1' : '')
-            cron('H 1 * * 1')
+            // cron('H 1 * * 1')
         }
         stages {
             stage('github url') {
