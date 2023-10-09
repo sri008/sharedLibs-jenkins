00849a56a205049a47781d2fc11e1da265ca2e48
Srikant Dhoundiyal <32190963+sri008@users.noreply.github.com>
2023-10-09 16:46:47 +0530

    Update cronJob.groovy

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -1,16 +1,14 @@
-// def call(Map params = [:]) {
 def call(body) {
+    def config =[:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
     body()
     pipeline {
         agent any
-        // environment{
-        //     GIT_URL = 'https://github.com/sri008/testinfra.git'
-        //     BRANCH_NAME = 'main'
-        // }
         triggers { 
-            cron( (env.BRANCH_NAME == 'main' && (env.GIT_URL.contains('infra') || env.GIT_URL.contains('infrastructure'))) ? 'H 1 * * 1' : '')
+            // cron( (env.BRANCH_NAME == 'main' && (env.GIT_URL.contains('infra') || env.GIT_URL.contains('infrastructure'))) ? 'H 1 * * 1' : '')
             // cron( (gitURL.contains('infra') || gitURL.contains('infrastructure')) ? 'H 1 * * 1' : '')
-            // cron('H 1 * * 1')
+            cron((env.BRANCH_NAME == 'main' && config.infra == 'true' ) : 'H 1 * * 1': '')
         }
         stages {
             stage('github url') {
