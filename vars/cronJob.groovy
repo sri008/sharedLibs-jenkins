847505b7a4992216ebff6ca2c7a59af4a529393b
sri008 <srikant_008@live.com>
2023-10-16 16:08:03 +0530

    added the script block inside pipeline

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -3,56 +3,56 @@ def call(body) {
 	body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    properties([
-        parameters([
-            [
-                $class: 'ChoiceParameter',
-                choiceType: 'PT_SINGLE_SELECT',
-                filterLength: 0, filterable: false,
-                name: 'ProjectName',
-                script: [
-                    $class: 'GroovyScript',
-                    script: 
-                        [
-                            classpath: [],  
-                            script:
-                                'return[\'SM\']'
-                        ]
-                ]
-            ],
-            [
-              $class: 'ChoiceParameter',
-              choiceType: 'PT_SINGLE_SELECT',
-              name: 'ScanType',
-              script: [
-                $class: 'GroovyScript',
-                script: [
-                    classpath: [],
-                    script:
-                        '''if(config.infra == null ) {
-                            return[\'Auto\',\'Manual\']   
-                        }'''
-                    ]
-              ]
-            ],
-            [
-                $class: 'CascadeChoiceParameter',
-                 choiceType: 'PT_SINGLE_SELECT',
-                name: 'Region',
-                referencedParameters: 'ScanType',
-                script: [
-                    $class: 'GroovyScript',
-                    script: 
-                        [classpath: [], 
-                        script:
-                            '''if (ScanType.equals("Auto")) {
-                              return ["ap-south-1"]
-                            }'''
-                        ]
-                ]
-            ]
-        ])
-    ])
+    // properties([
+    //     parameters([
+    //         [
+    //             $class: 'ChoiceParameter',
+    //             choiceType: 'PT_SINGLE_SELECT',
+    //             filterLength: 0, filterable: false,
+    //             name: 'ProjectName',
+    //             script: [
+    //                 $class: 'GroovyScript',
+    //                 script: 
+    //                     [
+    //                         classpath: [],  
+    //                         script:
+    //                             'return[\'SM\']'
+    //                     ]
+    //             ]
+    //         ],
+    //         [
+    //           $class: 'ChoiceParameter',
+    //           choiceType: 'PT_SINGLE_SELECT',
+    //           name: 'ScanType',
+    //           script: [
+    //             $class: 'GroovyScript',
+    //             script: [
+    //                 classpath: [],
+    //                 script:
+    //                     '''if(config.infra == null ) {
+    //                         return[\'Auto\',\'Manual\']   
+    //                     }'''
+    //                 ]
+    //           ]
+    //         ],
+    //         [
+    //             $class: 'CascadeChoiceParameter',
+    //              choiceType: 'PT_SINGLE_SELECT',
+    //             name: 'Region',
+    //             referencedParameters: 'ScanType',
+    //             script: [
+    //                 $class: 'GroovyScript',
+    //                 script: 
+    //                     [classpath: [], 
+    //                     script:
+    //                         '''if (ScanType.equals("Auto")) {
+    //                           return ["ap-south-1"]
+    //                         }'''
+    //                     ]
+    //             ]
+    //         ]
+    //     ])
+    // ])
 
     pipeline {
         agent any
@@ -60,13 +60,74 @@ def call(body) {
             cron((env.BRANCH_NAME == 'main' && !config.infra ) ? 'H 1 * * 1': '')
         }
         stages {
+            stage('set parameter') {
+                steps{
+                    script{
+                        def inputParams=[
+                        [
+                            $class: 'ChoiceParameter',
+                            choiceType: 'PT_SINGLE_SELECT',
+                            filterLength: 0, filterable: false,
+                            name: 'ProjectName',
+                            script: [
+                                $class: 'GroovyScript',
+                                script: 
+                                    [
+                                        classpath: [],  
+                                        script:
+                                            'return[\'SM\']'
+                                    ]
+                            ]
+                        ],
+                        [
+                          $class: 'ChoiceParameter',
+                          choiceType: 'PT_SINGLE_SELECT',
+                          name: 'ScanType',
+                          script: [
+                            $class: 'GroovyScript',
+                            script: [
+                                classpath: [],
+                                script:
+                                    '''if(config.buildDocker = 'true') {
+                                        return[\'Auto\',\'Manual\']   
+                                    } else {
+                                        return["Infra repo"]
+                                    }'''
+                                ]
+                          ]
+                        ],
+                        [
+                            $class: 'CascadeChoiceParameter',
+                             choiceType: 'PT_SINGLE_SELECT',
+                            name: 'Region',
+                            referencedParameters: 'ScanType',
+                            script: [
+                                $class: 'GroovyScript',
+                                script: 
+                                    [classpath: [], 
+                                    script:
+                                        '''if (ScanType.equals("Auto")) {
+                                          return ["ap-south-1"]
+                                        }'''
+                                    ]
+                            ]
+                        ]
+                    ]
+
+                    properties([
+                            parameters(inputParams)
+                        ])
+                    }
+                    
+                }
+            }
             stage('github url') {
                 steps {
                     echo "Git url --> ${env.GIT_URL}"
                     echo "Branch is used --> ${env.BRANCH_NAME}"
                     echo "is it infra or not --> ${config.infra}"
+                }
             }
         }
     }
-}
 }
