33ed99d614043c49460072552b62d9f8ce5c4dad
Srikant Dhoundiyal <32190963+sri008@users.noreply.github.com>
2023-11-02 13:43:27 +0530

    Update passVariable.groovy

--- vars/passVariable.groovy
+++ vars/passVariable.groovy
@@ -17,6 +17,7 @@ def call(body) {
             stage('github url') {
                 steps { 
 		    script {
+			    config.gitRepo = env.GIT_URL.replaceAll(/(https:\/\/github.com\/|\.git)/, "")
 			     childVar.childOne(config)
 			    // config.gitRepo = env.GIT_URL.replaceAll(/(https:\/\/github.com\/|\.git)/, "")
 			    // config.lastWord = config.gitRepo.contains("-cron-jobs")? "crons": config.gitRepo.endsWith("-new-notification") ? "wow" : config.gitRepo.split("[^\\w]+").last()
