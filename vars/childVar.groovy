0f23d86a1c6ed8d61d99c35df01225f0cc174e8f
Srikant Dhoundiyal <32190963+sri008@users.noreply.github.com>
2023-11-02 14:33:58 +0530

    Update childVar.groovy

--- vars/childVar.groovy
+++ vars/childVar.groovy
@@ -2,7 +2,7 @@ def childOne(config) {
 	// config.gitRepo = env.GIT_URL.replaceAll(/(https:\/\/github.com\/|\.git)/, "")
 	def lastWord = config.gitRepo.contains("-cron-jobs")? "crons": config.gitRepo.endsWith("-new-notification") ? "wow" : config.gitRepo.split("[^\\w]+").last()
 	echo "Debug: last word --> ${lastWord}"
-	withEnv(["LASTWORD=lastword"]){
+	withEnv(["LASTWORD=lastWord"]){
 		sh """echo last word --> $LASTWORD
                     echo ${config.gitRepo}"""
 	}
