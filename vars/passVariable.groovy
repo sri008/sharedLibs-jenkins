4357edaf80839665c5d64cbc818a4376bc23b87c
Srikant Dhoundiyal <32190963+sri008@users.noreply.github.com>
2023-11-02 12:04:25 +0530

    Update passVariable.groovy

--- vars/passVariable.groovy
+++ vars/passVariable.groovy
@@ -10,7 +10,6 @@ def call(body) {
     // config.repoName = env.GIT_URL.replaceAll("https://github.com","").replaceAll(".git", "")
     pipeline {
         agent any
-        config.repoName = env.GIT_URL.replaceAll("https://github.com","").replaceAll(".git", "")
         triggers { 
             cron((env.BRANCH_NAME == 'main' && !config.infra ) ? 'H 1 * * 1': '')
         }
@@ -18,15 +17,17 @@ def call(body) {
             stage('github url') {
                 steps { 
 		    script {
-			    if (config.repoName.contains("cron-jobs")) {
-				    config.lastword = 'crons'
-			    } else {
-				    config.lastword = config.repoName.split("[^\\w]+").last()
-			    }
+			    config.gitRepo = env.GIT_URL.reolaceAll(/(https:\/\/github.com\/|\.git)/, "")
+			    config.lastWord = config.gitRepo.contains("-cron-jobs")? "crons": config.gitRepo.endsWith("-new-notification") ? "wow"
+			    // if (config.repoName.contains("cron-jobs")) {
+				   //  config.lastword = 'crons'
+			    // } else {
+				   //  config.lastword = config.repoName.split("[^\\w]+").last()
+			    // }
 		    }
                     echo "Git url --> ${env.GIT_URL}"
 		    echo "repo Name --> ${config.repoName}"
-		    echo "last word --> ${config.lastword}"
+		    echo "last word --> ${config.lastWord}"
                 }
             }
         }
