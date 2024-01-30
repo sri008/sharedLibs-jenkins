fa61da89f88de5e645e6dfcfa7b96e2bb500bd25
Srikant Dhoundiyal <srikant_008@live.com>
2024-01-31 00:14:01 +0530

    debug 22 debug module repo

--- vars/subModule.groovy
+++ vars/subModule.groovy
@@ -1,22 +1,22 @@
 def call(body) {
-    pipeline {
-        agent any
-        stages {
-            stage('parent') {
-                steps {
-                    sh 'echo Heloo from child'
-                    sh 'ls -la'
-                    sh 'cat .git/config'
-                    sh 'cat .gitmodules'
-                }
-            }
-            stage('initialize sub module'){
-                steps{
-                    // sh 'ls -l'
-                    // sh 'git submodule init'
-                    // sh 'git submodule update'
-                    // sh 'ls -l'
-                    script {
+    // pipeline {
+    //     agent any
+    //     stages {
+    //         stage('parent') {
+    //             steps {
+    //                 sh 'echo Heloo from child'
+    //                 sh 'ls -la'
+    //                 sh 'cat .git/config'
+    //                 sh 'cat .gitmodules'
+    //             }
+    //         }
+    //         stage('initialize sub module'){
+    //             steps{
+    //                 // sh 'ls -l'
+    //                 // sh 'git submodule init'
+    //                 // sh 'git submodule update'
+    //                 // sh 'ls -l'
+    //                 script {
                          // Read the .gitmodules file and extract submodule paths
                         def gitmodulesContent = readFile('.gitmodules')
                         def submodulePaths = gitmodulesContent.readLines().findAll { it =~ /^\s*path\s*=/ }.collect { it.replaceFirst(/^\s*path\s*=\s*/, '') }
@@ -35,14 +35,14 @@ def call(body) {
                             ls -la
                             """
                         }
-                    }
-                }
-            }
-        }
-        post {
-            always {
-                cleanWs()
-            }
-        }
-    }
+//                     }
+//                 }
+//             }
+//         }
+//         post {
+//             always {
+//                 cleanWs()
+//             }
+//         }
+//     }
 }
