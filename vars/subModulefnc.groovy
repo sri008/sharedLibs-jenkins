6c9c59bf332e78c0bf17af7803f4bbd6ed7ca411
Srikant Dhoundiyal <srikant_008@live.com>
2024-01-31 21:51:55 +0530

    debug 52 debug Token

--- vars/subModulefnc.groovy
+++ vars/subModulefnc.groovy
@@ -1,5 +1,6 @@
 def call(body) {
     // Read the .gitmodules file and extract submodule paths
+    def github_token = credentials('testAPi')
     def gitB_name = env.GIT_BRANCH
     def gitmodulesContent = readFile('.gitmodules')
     def submodulePaths = gitmodulesContent.readLines().findAll { it =~ /^\s*path\s*=/ }.collect { it.replaceFirst(/^\s*path\s*=\s*/, '') }
@@ -25,7 +26,7 @@ def call(body) {
             cp ../*.tgz .
 			git status
 			git add . ; git commit -m "fix patch" ; git push --set-upstream origin "$gitB_name"; git push
-            curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer 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" -H "X-GitHub-Api-Version: 2022-11-28" \
+            curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${github_token}" -H "X-GitHub-Api-Version: 2022-11-28" \
                 -d '{"title": "Test automatic PR creation ", "head": "${gitB_name}-01", "base": "${baseBranch}", "body": ""}' \
                 https://api.github.com/repos/sri008/test-cron-jobs/pulls
         """
