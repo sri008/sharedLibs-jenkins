e2270851352acfca7883b51f412b4de7764e7220
Srikant Dhoundiyal <srikant_008@live.com>
2024-01-31 16:09:33 +0530

    debug 25 change quotes debug branch and PR creation

--- vars/subModulefnc.groovy
+++ vars/subModulefnc.groovy
@@ -1,6 +1,6 @@
 def call(body) {
     // Read the .gitmodules file and extract submodule paths
-    def gitB_name = env.GIT_BRANCH
+    // def gitB_name = env.GIT_BRANCH
     def gitmodulesContent = readFile('.gitmodules')
     def submodulePaths = gitmodulesContent.readLines().findAll { it =~ /^\s*path\s*=/ }.collect { it.replaceFirst(/^\s*path\s*=\s*/, '') }
     def baseBranch = gitmodulesContent.readLines().find { it =~ /branch\s*=/ }?.replaceAll(/^\s*branch\s*=\s*/, '') ?: 'main'
@@ -8,19 +8,19 @@ def call(body) {
     // Iterate through each submodule path
     submodulePaths.each { submodulePath ->
         sh "ls -l ${submodulePath.trim()}"
-        sh '''
+        sh """
             git config --list
             echo ##########
             git submodule sync ; git submodule update --init --recursive --remote
             cat .git/config
             cd ${submodulePath.trim()}
             ls -l 
-            git branch | grep -q  ${gitB_name}
+            git branch | grep -q  ${env.GIT_BRANCH}
             if [ $? -eq 0 ]; then
-                git checkout ${gitB_name}
+                git checkout  ${env.GIT_BRANCH}
             else
-                git checkout -b ${gitB_name}
+                git checkout -b  ${env.GIT_BRANCH}
             fi
-        '''
+        """
     }
 }
