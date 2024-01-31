c2d2aec0e1714643693d256059d27c9ae16faa7e
Srikant Dhoundiyal <srikant_008@live.com>
2024-01-31 19:27:09 +0530

    debug 45 debug Token

--- vars/subModulefnc.groovy
+++ vars/subModulefnc.groovy
@@ -25,7 +25,7 @@ def call(body) {
             cp ../*.tgz .
 			git status
 			git add . ; git commit -m "fix patch" ; git push --set-upstream origin "$gitB_name"; git push
-            curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer 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" -H "X-GitHub-Api-Version: 2022-11-28" \
+            curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer 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" -H "X-GitHub-Api-Version: 2022-11-28" \
                 -d '{"title": "Test automatic PR creation ", "head": "${gitB_name}-01", "base": "${baseBranch}", "body": ""}' \
                 https://api.github.com/repos/sri008/test-cron-jobs/pulls
         """
