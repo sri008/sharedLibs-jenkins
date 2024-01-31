54e5dc77b1e1b24dc96ca2c956cf27055c70ab24
Srikant Dhoundiyal <srikant_008@live.com>
2024-01-31 13:23:16 +0530

    debug 03 debug branch and PR creation

---
+++ vars/subModulefnc.groovy
@@ -0,0 +1,27 @@
+def call(body) {
+    // Read the .gitmodules file and extract submodule paths
+    def gitB_name = env.GIT_BRANCH
+    def gitmodulesContent = readFile('.gitmodules')
+    def submodulePaths = gitmodulesContent.readLines().findAll { it =~ /^\s*path\s*=/ }.collect { it.replaceFirst(/^\s*path\s*=\s*/, '') }
+    def baseBranch = gitmodulesContent.readLines().find { it =~ /branch\s*=/ }?.replaceAll(/^\s*branch\s*=\s*/, '') ?: 'main'
+
+    // Iterate through each submodule path
+    submodulePaths.each { submodulePath ->
+        sh "ls -l ${submodulePath.trim()}"
+        sh """
+            git config --list
+            echo ##########
+            git submodule sync
+            git submodule update --init --recursive --remote
+            cat .git/config
+            cd ${submodulePath.trim()}
+            git branch
+            git checkout -b env.GIT_BRANCH
+            cp ../*.tgz .
+            git status
+            git add . ; git commit -m "fix patch" ; git push 
+            
+            curl -X POST -u "sri008:Sri811kri$" -d '{"title": "Test automatic PR creation ", "head": "${gitB_name}-01", "base": "${baseBranch}", "body": ""}' https://api.github.com/repos/sri008/test-cron-jobs/pulls
+        """
+    }
+}
