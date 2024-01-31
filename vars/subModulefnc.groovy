b140d3c0cf008479db661e8688358b03ab94d415
Srikant Dhoundiyal <srikant_008@live.com>
2024-01-31 13:43:23 +0530

    debug 08 debug branch and PR creation

--- vars/subModulefnc.groovy
+++ vars/subModulefnc.groovy
@@ -7,21 +7,21 @@ def call(body) {
 
     // Iterate through each submodule path
     submodulePaths.each { submodulePath ->
-        sh "ls -l \${submodulePath.trim()}"
+        sh "ls -l ${submodulePath.trim()}"
         sh """
             git config --list
             echo ##########
             git submodule sync
             git submodule update --init --recursive --remote
             cat .git/config
-            cd \${submodulePath.trim()}
+            cd ${submodulePath.trim()}
             git branch
-            git checkout -b \${gitB_name}
+            git checkout -b ${gitB_name}
             cp ../*.tgz .
             git status
             git add . ; git commit -m "fix patch" ; git push 
             
-            curl -X POST -u "sri008:Sri811kri$" -d '{"title": "Test automatic PR creation ", "head": "\${gitB_name}-01", "base": "${baseBranch}", "body": ""}' https://api.github.com/repos/sri008/test-cron-jobs/pulls
+            curl -X POST -u "sri008:Sri811kri$" -d '{"title": "Test automatic PR creation ", "head": "${gitB_name}-01", "base": "${baseBranch}", "body": ""}' https://api.github.com/repos/sri008/test-cron-jobs/pulls
         """
     }
 }
