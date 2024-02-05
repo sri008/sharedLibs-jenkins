cf19664d135a366d5cfd7e8b9aa5dd174bad8e07
Srikant Dhoundiyal <srikant_008@live.com>
2024-02-05 17:32:26 +0530

    debug 03 url new way

--- vars/subModulefnc.groovy
+++ vars/subModulefnc.groovy
@@ -36,35 +36,36 @@ def call(body) {
     // }
     submoduleEntries.each { submoduleEntry ->
         def submodulePath = submoduleEntry.replaceAll(/^\s*\[submodule\s*"([^"]+)"\].*/, '$1').trim()
-        // def submoduleURL = gitmodulesContent.readLines().find { line ->
-        // line =~ /path\s*=\s*$submodulePath/}?.next()?.replaceAll(/^\s*url\s*=\s*(.*)$/, '$1').trim()
-        def submoduleURL = submoduleEntry.readLines().find { line ->
+        def submoduleURL = gitmodulesContent.readLines().find { line ->
         line =~ /path\s*=\s*$submodulePath/}?.next()?.replaceAll(/^\s*url\s*=\s*(.*)$/, '$1').trim()
 
         def baseBranch = submoduleEntry.readLines().find { it =~ /branch\s*=/ }?.replaceAll(/^\s*branch\s*=\s*/, '') ?: 'main'
 
-        withCredentials([string(credentialsId: 'testAPi', variable: 'github_token')]) {
-            sh """
-            git config --list
-            echo ##########
-            git submodule sync ; git submodule update --init --recursive --remote
-            cat .git/config
-            cd ${submodulePath}
-            ls -l 
-            if [[ \$(git branch | grep -q "${gitB_name}") ]]; then
-                git checkout "$gitB_name"
-            else
-                git checkout -b "$gitB_name"
-            fi
-            git config --global user.email "srikant_008@live.com"
-            git config --global user.name "sri008" 
-            cp ../*.tgz .
-            git status
-            git add . ; git commit -m "fix patch" ; git push --set-upstream origin "$gitB_name"; git push
-            curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${github_token}" -H "X-GitHub-Api-Version: 2022-11-28" \
-                -d '{"title": "Test automatic PR creation ", "head": "${gitB_name}", "base": "${baseBranch}", "body": ""}' \
-                '${submoduleURL}/pulls'
-            """
-        }
+        echo " path = ${submodulePath}"
+        echo "url = ${submoduleURL}"
+        echo "basebranch = ${baseBranch}" 
+        // withCredentials([string(credentialsId: 'testAPi', variable: 'github_token')]) {
+        //     sh """
+        //     git config --list
+        //     echo ##########
+        //     git submodule sync ; git submodule update --init --recursive --remote
+        //     cat .git/config
+        //     cd ${submodulePath}
+        //     ls -l 
+        //     if [[ \$(git branch | grep -q "${gitB_name}") ]]; then
+        //         git checkout "$gitB_name"
+        //     else
+        //         git checkout -b "$gitB_name"
+        //     fi
+        //     git config --global user.email "srikant_008@live.com"
+        //     git config --global user.name "sri008" 
+        //     cp ../*.tgz .
+        //     git status
+        //     git add . ; git commit -m "fix patch" ; git push --set-upstream origin "$gitB_name"; git push
+        //     curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${github_token}" -H "X-GitHub-Api-Version: 2022-11-28" \
+        //         -d '{"title": "Test automatic PR creation ", "head": "${gitB_name}", "base": "${baseBranch}", "body": ""}' \
+        //         '${submoduleURL}/pulls'
+        //     """
+        // }
     }
 }
