b3653c866bbdb5a6250e6b0ac9af6ca029eeba11
Srikant Dhoundiyal <srikant_008@live.com>
2024-02-05 19:34:28 +0530

    add debug file

---
+++ vars/debug_subModfnc.groovy
@@ -0,0 +1,18 @@
+def call(body){
+    def gitB_name = env.GIT_BRANCH
+    def gitmodulesContent = readFile('.gitmodules')
+    // Define a regular expression to match submodule entries
+    def regex = /\[submodule "(.*?)"]\n\s*path = (.*?)\n\s*url = (.*?)\n/
+    // Use a matcher to find all submodule entries
+    def matcher = (fileContents =~ regex)
+    // Iterate through matches and print path and url values
+    matcher.each { match ->
+        def submodule = [:]
+        submodule['path'] = match[2]
+        submodule['url'] = match[3]
+
+        println "Path: ${submodule['path']}"
+        println "URL: ${submodule['url']}"
+        println '---'
+    }
+}
